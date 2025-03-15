e775304fcb044ae49b4ad6dddef4a421d2db7e4b
claudemods <73653396+claudemods@users.noreply.github.com>
2025-03-15 03:45:14 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QComboBox>
+#include <QFile>
+#include <QWebEngineView>
+#include <QResource>
+#include <QDialog>
+
+class HtmlViewer : public QDialog {
+    Q_OBJECT
+
+public:
+    HtmlViewer(const QString &htmlContent, QWidget *parent = nullptr) : QDialog(parent) {
+        setWindowTitle("HTML Viewer");
+        setMinimumSize(800, 600);
+
+        QVBoxLayout *layout = new QVBoxLayout(this);
+
+        webView = new QWebEngineView(this);
+        webView->setHtml(htmlContent);
+        layout->addWidget(webView);
+
+        setLayout(layout);
+    }
+
+private:
+    QWebEngineView *webView;
+};
+
+class HtmlEditor : public QWidget {
+    Q_OBJECT
+
+public:
+    HtmlEditor(QWidget *parent = nullptr) : QWidget(parent) {
+        // Main layout
+        QVBoxLayout *layout = new QVBoxLayout(this);
+
+        // Logo
+        QLabel *logoLabel = new QLabel(this);
+        QPixmap logo(":/images/logo.png");
+        logoLabel->setPixmap(logo);
+        logoLabel->setAlignment(Qt::AlignCenter);
+        layout->addWidget(logoLabel);
+
+        // Text editor
+        textEdit = new QTextEdit(this);
+        textEdit->setStyleSheet("background-color: black; color: teal;");
+        layout->addWidget(textEdit);
+
+        // View HTML button
+        QPushButton *viewHtmlButton = new QPushButton("View Html", this);
+        viewHtmlButton->setStyleSheet("background-color: gold; color: teal;");
+        layout->addWidget(viewHtmlButton);
+
+        // Color scheme selector
+        QComboBox *colorSchemeCombo = new QComboBox(this);
+        colorSchemeCombo->addItem("Default (Black Background, Teal Text)");
+        colorSchemeCombo->addItem("Red Background, Gold Text");
+        colorSchemeCombo->addItem("Dark Green Background, Black Text");
+        colorSchemeCombo->addItem("Black Background, Teal Text");
+        layout->addWidget(colorSchemeCombo);
+
+        // Connect signals and slots
+        connect(viewHtmlButton, &QPushButton::clicked, this, &HtmlEditor::viewHtml);
+        connect(colorSchemeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &HtmlEditor::changeColorScheme);
+
+        setLayout(layout);
+    }
+
+private slots:
+    void viewHtml() {
+        QString htmlContent = textEdit->toPlainText();
+        HtmlViewer *viewer = new HtmlViewer(htmlContent, this);
+        viewer->exec(); // Open the viewer as a modal dialog
+    }
+
+    void changeColorScheme(int index) {
+        switch (index) {
+            case 0: // Default
+                textEdit->setStyleSheet("background-color: black; color: teal;");
+                break;
+            case 1: // Red Background, Gold Text
+                textEdit->setStyleSheet("background-color: red; color: gold;");
+                break;
+            case 2: // Dark Green Background, Black Text
+                textEdit->setStyleSheet("background-color: darkgreen; color: black;");
+                break;
+            case 3: // Black Background, Teal Text
+                textEdit->setStyleSheet("background-color: black; color: teal;");
+                break;
+        }
+    }
+
+private:
+    QTextEdit *textEdit;
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    // Load the logo from the resource file
+    QResource::registerResource("resources.rcc");
+
+    // Set application style
+    app.setStyleSheet("QWidget { background-color: teal; color: gold; }");
+
+    HtmlEditor editor;
+    editor.setWindowTitle("Apex Html Editor And Viewer v1.0");
+    editor.resize(800, 600);
+    editor.show();
+
+    return app.exec();
+}
+
+#include "main.moc"
---
+++ main.pro
@@ -0,0 +1,13 @@
+# Project name
+TARGET = AppLauncher
+
+# Required Qt modules
+QT += core gui widgets webenginewidgets webenginecore webenginequick
+
+# Source files
+SOURCES += main.cpp
+
+# C++ standard
+CONFIG += c++23
+
+RESOURCES += resources.qrc
